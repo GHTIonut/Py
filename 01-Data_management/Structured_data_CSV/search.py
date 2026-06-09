d875bcfdb495bf2c89c065bb39af5cc7cb446e39
Ghita Ionut <ionut996ght@gmail.com>
2026-06-09 20:18:12 +0300

    Implement keyword search across csv rows.

---
+++ 01-Data_management/Structured_data_CSV/search.py
@@ -0,0 +1,21 @@
+import csv
+
+with open("sales.csv", "r", encoding="utf-8") as file:
+    
+    reader = csv.reader(file)
+    header = next(reader)
+    search = input("Search: ").strip().lower()
+
+    # Solution no.1 
+    # Finds the list that contains user's input.
+    
+    for row in reader:
+        for cell in row:
+            if search in cell.lower():
+                print(row)
+            
+    # Solution no.2:
+    
+    # for row in reader:
+    #     if any(search in cell.lower() for cell in row):
+    #          print(row)
